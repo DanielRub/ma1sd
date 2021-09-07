33459362a7bf6d568357c1a20c7e55cad475c9a8
daniel rubambura <danielrubambura@gmail.com>
2021-09-07 15:08:27 +0200

    send whatsapp message

--- src/main/java/io/kamax/matrix/client/regular/MatrixHttpClient.java
+++ src/main/java/io/kamax/matrix/client/regular/MatrixHttpClient.java
@@ -181,11 +181,12 @@ public class MatrixHttpClient extends AMatrixHttpClient implements _MatrixClient
     @Override
     public void login(MatrixPasswordCredentials credentials) {
         URL url = getClientPath("login");
-
+        log.info("url="+url.toString());
         LoginPostBody data = new LoginPostBody(credentials.getLocalPart(), credentials.getPassword());
+        log.info("data:"+data.toString());
         getDeviceId().ifPresent(data::setDeviceId);
         Optional.ofNullable(context.getInitialDeviceName()).ifPresent(data::setInitialDeviceDisplayName);
-
+        log.info("body:"+getJsonBody(data));
         updateContext(execute(new Request.Builder().post(getJsonBody(data)).url(url)));
     }
 
